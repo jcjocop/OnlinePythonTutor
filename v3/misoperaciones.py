693af13d0e85133eb2c2a38f383660ff07bded3b
Juan Carlos Jocop Arriola <jcjocop@gmail.com>
2015-03-05 22:30:39 -0600

    misoperaciones.py
    contiene tres funciones a ser evaluadas por otro script.

---
+++ v3/misoperaciones.py
@@ -0,0 +1,15 @@
+#
+def sum(valor1, valor2, valor3):
+  lasuma_es = valor1 + valor2 + valor3
+  return lasuma_es
+
+#
+def promedio(lasuma):
+  elpromedio_es = lasuma/3.0
+  return elpromedio_es
+
+#
+def despedida()
+  print ""
+  print "fin del programa"
+  
